c056ae0dd33ec031a1ba802cf3bb205c68760819
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-07-09 11:32:47 +0200

    Add project files.

---
+++ IdentityProxy.Api/Identity/IdentityService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+namespace IdentityProxy.Api.Identity;
+
+public class IdentityService
+{
+    private readonly IMemoryCache _cache;
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<IdentityService> _logger;
+    
+    
+}
